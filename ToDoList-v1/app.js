7da9fb6128660faafce6559b4351340a6944de09
Mitranjay Krishna <mitranjaykrishna@gmail.com>
2022-08-09 20:02:57 +0530

    TodoList updated

--- ToDoList-v1/app.js
+++ ToDoList-v1/app.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const bodyPareser=require("body-parser");
 const mongoose=require("mongoose");
+var _ = require('lodash');
 
 const app=express();
 
@@ -28,20 +29,39 @@ const item3=new Item({
 });
 const defaultItems=[item1,item2,item3];
 
-Item.insertMany(defaultItems,function(err)
-{
-    if(err)
-    console.log(err);
-    else
-    console.log("Succesfully stored");
-});
- 
+const listSchema=new mongoose.Schema(
+    {
+        name: String,
+        items: [itemSchema]
+    }
+);
 
-// let items=[];
-// let works=[];
+const List=mongoose.model("List",listSchema);
+
+
+
+ 
 
 app.get("/",function(req,res)
 {
+    Item.find({},function(err,foundItems){
+        if(foundItems.length===0)
+        {
+
+            Item.insertMany(defaultItems,function(err)
+            {
+                if(err)
+                console.log(err);
+                else
+                console.log("Succesfully stored");
+            });
+            res.redirect("/");
+        }
+        else{
+            res.render("list",{listTitle: "Today", allItems: foundItems});
+        }
+    });
+
     let today=new Date();
     let option={
         weekday: "long",
@@ -49,29 +69,111 @@ app.get("/",function(req,res)
         month: "long"
     };
     let day=today.toLocaleDateString("en-us",option);
-    res.render("list",{listTitle: "Today", allItems: items});
+    // res.render("list",{listTitle: "Today", allItems: items});
     
 })
 
+// app.get("/work", function(req,res)
+// {
+//     res.render("list",{listTitle: "WorkPlace", allItems: works});
+
+// });
+
+app.get("/:newList",function(req,res)
+{
+    const customList=_.capitalize(req.params.newList);
+
+    
+    // list.save();
+
+    List.findOne({name: customList},function(err,foundList){
+        if(!err)
+        {
+            if(!foundList)
+            {
+                const list= new List(
+                    {
+                        name: customList,
+                        items: defaultItems
+                    }
+                ) ;      
+                list.save();
+                res.redirect("/"+customList);
+            }
+            else
+            {
+                res.render("list",{listTitle: foundList.name, allItems: foundList.items});
+            }
+        }
+    });   
+});
+
 app.post("/",function(req,res)
 {
-    let item=req.body.item;
-    if(req.body.list==="WorkPlace")
+    const itemName=req.body.item;
+    const listName=req.body.list;
+    const newItem=new Item({
+        name: itemName
+    });
+
+    if(listName==="Today")
     {
-        works.push(item);
-        res.redirect("/work");
-    }
-    else{
-        items.push(item);
+        newItem.save();
         res.redirect("/");
     }
+
+    else{
+        List.findOne({name: listName}, function(err,foundList)
+        {
+            foundList.items.push(newItem);
+            foundList.save();
+            res.redirect("/"+listName);
+        });
+    }
+    // let item=req.body.item;
+    // if(req.body.list==="WorkPlace")
+    // {
+    //     works.push(item);
+    //     res.redirect("/work");
+    // }
+    // else{
+    //     items.push(item);
+    //     res.redirect("/");
+    // }
+});
+
+app.post("/delete",function(req,res){
+    
+    const removeId=req.body.checkbox;
+    const listName=req.body.listName;
+
+    if(listName==="Today")
+    {
+        Item.findByIdAndRemove(removeId,function(err){
+            if(err)
+            console.log(err);
+            else
+            console.log("Deleted From DB");
+        });
+
+        res.redirect("/");
+    }
+      else
+      {
+        List.findOne({name: listName}, function(err,foundList)
+        {
+            List.findOneAndUpdate({name: listName},{$pull: {items: {_id: removeId}}},function(err,foundList){
+                if(!err)
+                {
+                    res.redirect("/"+listName);
+                }
+            });
+        });
+      }   
+    
 })
 
-app.get("/work", function(req,res)
-{
-    res.render("list",{listTitle: "WorkPlace", allItems: works});
 
-});
 
 
 
