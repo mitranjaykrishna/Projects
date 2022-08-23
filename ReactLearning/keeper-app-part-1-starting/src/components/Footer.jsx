8106f5d4bad6f51de6b267c7e3b3df6af959da9f
Mitranjay Krishna <mitranjaykrishna@gmail.com>
2022-08-23 19:09:26 +0530

    React Props Practice   And add React DevTools File

--- ReactLearning/keeper-app-part-1-starting/src/components/Footer.jsx
+++ ReactLearning/keeper-app-part-1-starting/src/components/Footer.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 function Footer()
 {
-    return <div><footer><p>Copyright © 2022</p></footer></div>;
+    const currentYear=new Date().getFullYear();
+    return <div><footer><p>Copyright © {currentYear}</p></footer></div>;
 }
 
 export default Footer;
---
+++ ReactLearning/react-props-practice-forked/src/components/Card.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Card(props)
+{
+  return (
+    
+    <div className="card">
+        <div className="top">
+          <h2 className="name">{props.name}</h2>
+          <img
+            src={props.img}
+            alt="avatar_img"
+            className="circle-img"
+          />
+        </div>
+        <div className="bottom">
+          <p className="info">{props.phone}</p>
+          <p className="info">{props.email}</p>
+        </div>
+      </div>
+  );
+}
+
+export default Card;
